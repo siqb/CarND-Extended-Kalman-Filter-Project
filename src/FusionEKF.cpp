c8625e8c26ce4237ccc539599accb80eff37373c
siqb <iqbals84@gmail.com>
2018-11-12 06:28:23 -0800

    Set noise, process covariance matrix.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -176,11 +176,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
                    0, 1, 0, dt,
                    0, 0, 1, 0,
 		   0, 0, 0, 1;
+        
+        // Set Noise 
+        float noise_ax = 9.0;
+        float noise_ay = 9.0;	
+        //set the process covariance matrix Q
 	float dt_2 = dt * dt;
 	float dt_3 = dt_2 * dt;
 	float dt_4 = dt_3 * dt;
-
-	//set the process covariance matrix Q
 	ekf_.Q_ = MatrixXd(4, 4);
 	ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
 			   0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,

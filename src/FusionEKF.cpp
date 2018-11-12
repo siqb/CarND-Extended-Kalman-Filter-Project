b950f73b3ad617c3161ac9c1b6f269d0eab121a4
siqb <iqbals84@gmail.com>
2018-11-12 06:27:49 -0800

    Modify F matrix.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -166,13 +166,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
 	previous_timestamp_ = measurement_pack.timestamp_;
 
+
+
+	//Modify the F matrix so that the time is integrated
+	//ekf_.F_(0, 2) = dt;
+	//ekf_.F_(1, 3) = dt;
+	ekf_.F_ = MatrixXd(4, 4);
+	ekf_.F_ << 1, 0, dt, 0,
+                   0, 1, 0, dt,
+                   0, 0, 1, 0,
+		   0, 0, 0, 1;
 	float dt_2 = dt * dt;
 	float dt_3 = dt_2 * dt;
 	float dt_4 = dt_3 * dt;
-
-	//Modify the F matrix so that the time is integrated
-	ekf_.F_(0, 2) = dt;
-	ekf_.F_(1, 3) = dt;
 
 	//set the process covariance matrix Q
 	ekf_.Q_ = MatrixXd(4, 4);

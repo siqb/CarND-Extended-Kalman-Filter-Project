e0f24b0ba79f0955422e953ac632eb7cb3356bff
siqb <iqbals84@gmail.com>
2018-11-12 13:19:28 -0800

    Kalman filter equations from course.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +35,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = P_ - K * H_*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +55,30 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  float rho = sqrt( px*px + py*py );
+
+  // When rho is 0 skip update.
+  if(fabs(rho) < .0001){
+    return;
+  }
+  VectorXd h_x(3);
+  h_x << rho, atan2( py, px ), ( px*vx + py*vy )/rho;
+
+  VectorXd y = z - h_x;
+  //float pi=3.141592;
+  //if( y[1] > pi )
+  //  y[1] =y[1]- 2.0*pi;
+  //if( y[1] < -pi )
+  //  y[1] = y[1]+ 2.0*pi;
+  MatrixXd S = H_*P_*H_.transpose() + R_;
+  MatrixXd Sinv = S.inverse();
+  MatrixXd K =  P_*H_.transpose()*Sinv;
+
+  // Compute new state
+  x_ = x_ + K*y;
+  P_ = P_ - K*H_*P_;
 }

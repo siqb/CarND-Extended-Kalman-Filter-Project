b4cf41dc426683ddb93f136d22e8c32d40e0946f
siqb <iqbals84@gmail.com>
2018-11-12 13:22:39 -0800

    Put RMSE function from class in tools.

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,24 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  if(estimations.size() == 0){
+    cout << "Input is empty" << endl;
+    return rmse;
+  }
+  if(estimations.size() != ground_truth.size()){
+    cout << "Invalid estimation or ground_truth. Data should have the same size" << endl;
+    return rmse;
+  }
+  for(unsigned int i=0; i < estimations.size(); ++i){
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
